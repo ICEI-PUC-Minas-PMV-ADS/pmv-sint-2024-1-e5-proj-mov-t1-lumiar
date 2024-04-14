ced1ec710705d746183247c007bb9fe12433b36f
unknown <cristhyamparaiso@gmail.com>
2024-04-14 15:16:18 -0300

    refactor: add content index js to app js

--- src/back-end/app.js
+++ src/back-end/app.js
@@ -1,6 +1,25 @@
-const express = require("express");
+const express = require('express')
+const godfatherRoutes = require('./src/routes/godfather')
+const institutionRoutes = require('./src/routes/institution')
 
-const app = express();
-app.use(express.json());
+const app = express()
 
-module.exports = app;
+function onAuthenticate(req, res, next) {
+    const apiKey = req.headers['x-api-key']
+    if (!apiKey || apiKey !== process.env.X_API_KEY) {
+        return res.status(401).json({ erro: 'Chave de API inválida ou ausente' })
+    }
+    next()
+}
+
+app.use(express.json())
+app.use(onAuthenticate)
+
+app.use(godfatherRoutes)
+app.use(institutionRoutes)
+
+app.listen(8000, () => {
+    console.log('Servidor está rodando na porta 8000')
+})
+
+module.exports = app
