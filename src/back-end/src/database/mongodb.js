c102e0c85a34a9798cf2428fc3e1f990fbd78595
unknown <cristhyamparaiso@gmail.com>
2024-04-14 15:19:18 -0300

    refactor: refactor connection in mongodb database

--- src/back-end/src/database/mongodb.js
+++ src/back-end/src/database/mongodb.js
@@ -1,21 +1,10 @@
-const mongoose = require("mongoose");
+require('dotenv').config()
+const mongoose = require('mongoose')
 
 class MongoDb {
-  static createConnection() {
-    const options = {
-      serverSelectionTimeoutMS: 15000,
-      socketTimeoutMS: 20000,
-      connectTimeoutMS: 15000,
-      maxPoolSize: 1,
-      minPoolSize: 1,
-      family: 4,
-    };
-
-    return mongoose.createConnection(
-      process.env.MONGO_STRING_CONNECTION,
-      options
-    );
-  }
+    static createConnection() {
+        return mongoose.createConnection(process.env.MONGO_STRING_CONNECTION)
+    }
 }
 
-module.exports = MongoDb;
+module.exports = MongoDb
