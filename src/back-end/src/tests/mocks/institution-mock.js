0c96a5fae0d8c0ad511bc20cc33c31e3083c516d
unknown <cristhyamparaiso@gmail.com>
2024-04-14 15:24:23 -0300

    test: add institution mock test

---
+++ src/back-end/src/tests/mocks/institution-mock.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose')
+
+module.exports = {
+    institutionMock: {
+        name: 'Test User',
+        password: 'password123',
+        creationDate: new Date('2023-01-01').toISOString(),
+        email: 'test@example.com',
+        cnpj: '12345678901234',
+        children: {
+            John: new mongoose.Types.ObjectId(),
+            Patrick: new mongoose.Types.ObjectId(),
+        },
+        description: 'Lorem ipsum dolor sit amet',
+        address: {
+            street: '123 Main St',
+            district: 'Downtown',
+            state: 'California',
+            country: 'USA',
+            cep: '12345-678',
+        },
+        affiliation: true,
+        createdAt: new Date('2023-01-01').toISOString(),
+        updatedAt: null,
+    },
+}
