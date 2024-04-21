f2d64e6b9d3317e6819c60552fd4461ed94a7b7c
unknown <cristhyamparaiso@gmail.com>
2024-04-21 13:13:32 -0300

    feat: add new routes on donation

--- src/back-end/src/routes/donation.js
+++ src/back-end/src/routes/donation.js
@@ -4,5 +4,7 @@ const Donation = require('../handlers/donation-handler')
 const routes = Router()
 
 routes.post('/create-donation', Donation.createDocument)
+routes.get('/get-donation/sponsor/:id', Donation.getAllDonationByIdSponsor)
+routes.get('/get-donation/child/:id', Donation.getAllDonationByIdChild)
 
 module.exports = routes
--- src/back-end/src/routes/institution.js
+++ src/back-end/src/routes/institution.js
@@ -3,12 +3,12 @@ const Institution = require('../handlers/institution-handler')
 
 const routes = Router()
 
-routes.post('/institution', Institution.createDocument)
-routes.post('/send-email', Institution.sendEmail)
-routes.post('/auth/institution/login', Institution.authLogin)
 routes.get('/institution', Institution.getAllDocument)
 routes.get('/institution/:id', Institution.getDocumentById)
 routes.get('/institution/name/:name', Institution.getByName)
+routes.post('/institution', Institution.createDocument)
+routes.post('/send-email', Institution.sendEmail)
+routes.post('/auth/institution/login', Institution.authLogin)
 routes.put('/institution/:id', Institution.updateDocument)
 routes.delete('/institution/:id', Institution.delDocumentById)
 
