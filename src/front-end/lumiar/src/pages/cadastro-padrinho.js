56e1815d10a8d301bbd325e767b95473de1bfe34
leticia.lopes <leticia.rosedesantana@gmail.com>
2024-04-21 00:05:50 -0300

    Feat: add haveAccount, useNavigation

--- src/front-end/lumiar/src/pages/cadastro-padrinho.js
+++ src/front-end/lumiar/src/pages/cadastro-padrinho.js
@@ -1,82 +1,89 @@
-import { useState } from 'react';
-import { View, Text, TextInput, TouchableOpacity, StyleSheet, Image, KeyboardAvoidingView, Platform } from 'react-native';
-import {TextInputMask} from 'react-native-masked-text';
+import React, { useState } from 'react';
+import { ScrollView, View, Text, TextInput, TouchableOpacity, StyleSheet, Image, KeyboardAvoidingView, Platform } from 'react-native';
+import { TextInputMask } from 'react-native-masked-text';
+import { useNavigation } from '@react-navigation/native';
 
 export default function CadastroPadrinho() {
+    const navigation = useNavigation();
     const [cpf, setCpf] = useState('');
+
     return (
-
         <KeyboardAvoidingView behavior={Platform.OS === 'ios' ? 'padding' : 'height'} style={styles.container}>
+            <ScrollView contentContainerStyle={styles.scrollContainer} showsVerticalScrollIndicator={false}>
+                <Image
+                    source={require('../../assets/Lumiar_branco.png')}
+                    style={styles.imageLogo}
+                />
+                <View style={styles.formContainer}>
+                    <Text style={styles.signUpTxt}>Cadastre-se</Text>
 
-            <Image
-                source={require('../../assets/Lumiar_branco.png')}
-                style={styles.imageLogo}
-            />
+                    <View style={styles.inputView}>
+                        <TextInput
+                            style={styles.inputText}
+                            placeholder="Nome"
+                            keyboardType='default'
+                        />
+                    </View>
 
-            <View style={styles.formContainer}>
-                <Text style={styles.signUpTxt}>Cadastre-se</Text>
+                    <View style={styles.inputView}>
+                        <TextInput
+                            style={styles.inputText}
+                            placeholder="E-mail"
+                            keyboardType='email-address'
+                        />
+                    </View>
 
-                <View style={styles.inputView}>
-                    <TextInput
-                        style={styles.inputText}
-                        placeholder="Nome"
-                        keyboardType='default'
-                    />
+                    <View style={styles.inputView}>
+                        <TextInputMask
+                            style={styles.inputText}
+                            placeholder="CPF"
+                            keyboardType='numeric'
+                            type={'cpf'}
+                            value={cpf}
+                            onChangeText={text => setCpf(text)}
+                        />
+                    </View>
+
+                    <View style={styles.inputView}>
+                        <TextInput
+                            style={styles.inputText}
+                            placeholder="Data de nascimento"
+                            keyboardType='numeric'
+                        />
+                    </View>
+
+                    <View style={styles.inputView}>
+                        <TextInput
+                            style={styles.inputText}
+                            placeholder="Senha"
+                            keyboardType='default'
+                            secureTextEntry={true}
+                        />
+                    </View>
+
+                    <View style={styles.inputView}>
+                        <TextInput
+                            style={styles.inputText}
+                            placeholder="Confirme sua senha"
+                            keyboardType='default'
+                            secureTextEntry={true}
+                        />
+                    </View>
+
+                    <TouchableOpacity style={styles.btnRegister}>
+                        <Text style={styles.registerTextBtn}>Cadastrar</Text>
+                    </TouchableOpacity>
+
+                    <TouchableOpacity>
+                        <Text
+                            style={styles.haveAccount}
+                            onPress={() => navigation.navigate('Login')}>
+                            Já possui conta? Clique aqui.
+                        </Text>
+                    </TouchableOpacity>
                 </View>
-
-                <View style={styles.inputView}>
-                    <TextInput
-                        style={styles.inputText}
-                        placeholder="E-mail"
-                        keyboardType='email-address'
-                    />
-                </View>
-
-                <View style={styles.inputView}>
-                    <TextInputMask 
-                    style={styles.inputText}
-                    placeholder="CPF"
-                    keyboardType='numeric'
-                    type={'cpf'}
-                    value={cpf}
-                    onChangeText={text => setCpf(text)}
- 
-                    />
-                </View>
-
-                <View style={styles.inputView}>
-                    <TextInput
-                        style={styles.inputText}
-                        placeholder="Data de nascimento"
-                        keyboardType='numeric'
-                    />
-                </View>
-
-                <View style={styles.inputView}>
-                    <TextInput
-                        style={styles.inputText}
-                        placeholder="Senha"
-                        keyboardType='default'
-                        secureTextEntry={true}
-                    />
-                </View>
-
-                <View style={styles.inputView}>
-                    <TextInput
-                        style={styles.inputText}
-                        placeholder="Confirme sua senha"
-                        keyboardType='default'
-                        secureTextEntry={true}
-                    />
-                </View>
-
-                <TouchableOpacity style={styles.btnRegister}>
-                    <Text style={styles.registerTextBtn}>Cadastrar</Text>
-                </TouchableOpacity>
-            </View>
-
+            </ScrollView>
         </KeyboardAvoidingView>
-
     );
 };
 
@@ -87,13 +94,19 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'center'
     },
+    scrollContainer: {
+        flexGrow: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        paddingBottom: 50 
+    },
     imageLogo: {
         width: 300,
         height: 300,
         resizeMode: 'contain'
     },
     formContainer: {
-        width: '80%',
+        width: '100%', 
         backgroundColor: '#ffffff',
         alignItems: 'center',
         borderRadius: 25,
@@ -106,6 +119,7 @@ const styles = StyleSheet.create({
         shadowRadius: 3.84,
         elevation: 5,
         marginBottom: 60,
+        paddingBottom: 20 
     },
     signUpTxt: {
         fontWeight: 'regular',
@@ -121,7 +135,8 @@ const styles = StyleSheet.create({
         height: 50,
         marginBottom: 20,
         justifyContent: 'center',
-        padding: 20
+        padding: 20,
+        alignSelf: 'center' 
     },
     inputText: {
         height: 50,
@@ -139,4 +154,9 @@ const styles = StyleSheet.create({
     registerTextBtn: {
         color: '#ffffff'
     },
+    haveAccount: {
+        color: '#007bff',
+        fontSize: 14,
+        marginBottom: 10
+    }
 });
