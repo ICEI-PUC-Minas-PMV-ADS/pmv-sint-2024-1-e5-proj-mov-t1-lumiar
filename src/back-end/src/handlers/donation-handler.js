d9b2d1cea037561b702f6d5fcc25465b2687dec5
unknown <cristhyamparaiso@gmail.com>
2024-04-21 13:12:58 -0300

    feat: add new endpoint to handlers of donation

--- src/back-end/src/handlers/donation-handler.js
+++ src/back-end/src/handlers/donation-handler.js
@@ -1,3 +1,5 @@
+const mongoose = require('mongoose')
+
 const DonationModel = require('../models/donation')
 const { validationDate } = require('../validation')
 const DonationService = require('../services/donation-service')
@@ -17,14 +19,40 @@ class Donation {
         return data
     }
 
+    static async getAllDonationByIdSponsor(req, res) {
+        const { id: sponsorId } = req.params
+
+        if (!sponsorId || !mongoose.Types.ObjectId.isValid(sponsorId)) {
+            return res.status(404).json({
+                message: 'Id invalido ou inexistente, gentileza informar',
+            })
+        }
+
+        const donations = await DonationModel.find({ sponsor: sponsorId }).lean()
+        return res.status(200).json({ data: donations })
+    }
+
+    static async getAllDonationByIdChild(req, res) {
+        const { id: childId } = req.params
+
+        if (!childId || !mongoose.Types.ObjectId.isValid(childId)) {
+            return res.status(404).json({
+                message: 'Id invalido ou inexistente, gentileza informar',
+            })
+        }
+
+        const donations = await DonationModel.find({ child: childId }).lean()
+        return res.status(200).json({ data: donations })
+    }
+
     static async createDocument(req, res) {
         const body = req.body
-        const { child, godfather } = body
+        const { child, sponsor } = body
 
         try {
             Donation.validateAndFormatDonation(body)
 
-            if ((await DonationService.existChild(child)) && (await DonationService.existGodfather(godfather))) {
+            if ((await DonationService.existChild(child)) && (await DonationService.existSponsor(sponsor))) {
                 const createdDonation = await DonationModel.create(body)
                 return res.status(200).json({ data: createdDonation })
             }
