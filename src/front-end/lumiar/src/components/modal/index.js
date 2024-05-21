a2f033871488db638260956d0740d63ad6b208a2
cristhyam-augusto <cristhyamparaiso@gmail.com>
2024-05-21 00:22:21 -0300

    feat: create modal to children detail

---
+++ src/front-end/lumiar/src/components/modal/index.js
@@ -0,0 +1,98 @@
+import { View, Text, Modal, Pressable, Image, TouchableOpacity } from 'react-native'
+
+export function KidModal({ onClose, isVisible, item = {
+  url: "https://blog-leiturinha-novo.s3.us-east-1.amazonaws.com/production/uploads/2022/07/iStock-1302266351-2-1.jpg",
+  kidAge: '10',
+  kidName: 'Lucas',
+  kidText: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sagittis odio nulla, id varius nisl scelerisque ac. Suspendisse nec tortor ac enim mattis sollicitudin. Vivamus eu venenatis leo. Vivamus porttitor euismod justo ultrices sagittis. Nulla porttitor porta sem, nec efficitur orci interdum in. Aliquam maximus, mi non convallis mollis, justo velit rutrum nisi, sed bibendum nisi augue at turpis. Phasellus vitae dapibus erat. Quisque laoreet justo a arcu vulputate, eu gravida neque posuere. Suspendisse imperdiet ligula a elementum vestibulum. Nulla dignissim dictum sapien, sit amet porttitor nisi convallis id. Sed ac ornare libero. Fusce eu ultrices orci. In hac habitasse platea dictumst. Suspendisse ut congue magna, non vulputate nibh. Morbi non sapien nisl. Fusce placerat faucibus neque, vitae congue felis condimentum ac."
+} }) {
+
+  return (
+    <Modal
+      dismiss={onClose}
+      animationType="slide"
+      transparent={true}
+      visible={isVisible}
+      onRequestClose={onClose}>
+      <TouchableOpacity style={{
+        flex: 1, justifyContent: "center", alignItems: "center", backgroundColor: 'rgba(0,0,0,0.5)',
+      }} onPress={onClose}>
+
+        <View style={styles.modalView}>
+          <Text style={styles.kidTitleName}>
+            {item.kidName}
+          </Text>
+          <Text style={styles.kidAge}>{`${item.kidAge} anos`}</Text>
+          <View style={styles.kidImageContainer}>
+            <Image
+              source={item.url}
+              style={styles.kidImage}
+            />
+          </View>
+          <Text style={styles.kidSubtitle}>Um pouco da minha história</Text>
+          <Text style={styles.kidText}>{item.kidText}</Text>
+          <Pressable style={styles.button}>
+            <Text style={styles.textStyle}>Apadrinhar</Text>
+          </Pressable>
+        </View>
+      </TouchableOpacity>
+    </Modal >
+  );
+};
+
+const styles = {
+  kidImageContainer: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  kidImage: {
+    width: '100%',
+    height: 162,
+    borderRadius: '5px',
+    marginBottom: '12px'
+  },
+  kidTitleName: {
+    color: '#51782C',
+    fontSize: '20px',
+    marginBottom: '8px'
+  },
+  kidAge: {
+    color: '#000000',
+    fontSize: '16px',
+    marginBottom: '8px'
+  },
+  kidSubtitle: {
+    color: '#51782C',
+    fontSize: '16px',
+    marginBottom: '8px'
+  },
+  kidText: {
+    color: '#000000',
+    fontSize: '13px',
+    marginBottom: '8px'
+  },
+  modalView: {
+    margin: 24,
+    background: '#FFF',
+    borderRadius: '5px',
+    padding: 12,
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  button: {
+    marginTop: '24px',
+    background: '#51782C',
+    width: '180px',
+    borderRadius: 20,
+    alignSelf: 'center',
+    padding: 10,
+    elevation: 2,
+  },
+  textStyle: {
+    color: 'white',
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+
+}
